07f6bc6cf45df8104455a2483da9da784e81cad1
Juan <0262576@up.edu.mx>
2025-10-28 14:41:49 -0600

    Added db_manager and insert user function

---
+++ videgameforum_backend/repository/db_manager.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"log"
+
+	"github.com/Lolsalas/GameForum/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DBManager struct {
+	orm *gorm.DB
+}
+
+func New(url string) *DBManager {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return &DBManager{db}
+}
+
+func (db *DBManager) InsertNewUser(name string, password string, email string) error {
+	user := models.Users{Username: name, Password: password, Email: email}
+	result := db.orm.Create(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
